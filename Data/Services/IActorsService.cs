8d9db77489a09cc78621fde18d74071a5fef90a6
Muratbek Sharibekov <msharibekov@one.kz>
2022-05-28 11:33:40 +0600

    Refactoring with base repository

--- Data/Services/IActorsService.cs
+++ Data/Services/IActorsService.cs
@@ -1,13 +1,9 @@
-﻿using Cinema.Models;
+﻿using Cinema.Data.Base;
+using Cinema.Models;
 
 namespace Cinema.Data.Services
 {
-    public interface IActorsService
+    public interface IActorsService : IEntityBaseRepository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
-        Task AddAsync(Actor actor);
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-        Task DeleteAsync(int id);
     }
 }
