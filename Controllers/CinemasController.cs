4071989265af8dd355fd1d818a9246406983f91c
Muratbek Sharibekov <msharibekov@one.kz>
2022-05-28 14:27:34 +0600

    Details about Cinema

--- Controllers/CinemasController.cs
+++ Controllers/CinemasController.cs
@@ -33,5 +33,12 @@ namespace Cinema.Controllers
             await _service.AddAsync(cinema);
             return RedirectToAction(nameof(Index));
         }
+        
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
+        }
     }
 }
